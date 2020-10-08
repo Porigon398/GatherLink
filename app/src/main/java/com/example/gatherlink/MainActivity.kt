77dc3d5018f20555b466380a7ff18914148e81bc
Porigon <tsukune398@gmail.com>
2020-10-08 21:26:52 +0900

    initial commit

---
+++ app/src/main/java/com/example/gatherlink/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gatherlink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
