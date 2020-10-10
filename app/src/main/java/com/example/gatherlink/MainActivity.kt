ba18ced9f79f39ac98371be512896037cc2164e8
Porigon <tsukune398@gmail.com>
2020-10-10 22:58:52 +0900

    add WebView

--- app/src/main/java/com/example/gatherlink/MainActivity.kt
+++ app/src/main/java/com/example/gatherlink/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.gatherlink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webview.webViewClient = WebViewClient()
+        webview.loadUrl("http://www.google.com")
     }
 }
