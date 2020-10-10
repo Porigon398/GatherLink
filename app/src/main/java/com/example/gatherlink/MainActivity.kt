7824cd71e4176a4190ed3195c78dab7708d41490
Porigon <tsukune398@gmail.com>
2020-10-10 23:53:11 +0900

    add comments...

--- app/src/main/java/com/example/gatherlink/MainActivity.kt
+++ app/src/main/java/com/example/gatherlink/MainActivity.kt
@@ -5,13 +5,17 @@ import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-
+/**
+　* Main Activity.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // OMAJINAI
         webview.webViewClient = WebViewClient()
+        // set URL want to open in WebView
         webview.loadUrl("http://www.google.com")
     }
 }
