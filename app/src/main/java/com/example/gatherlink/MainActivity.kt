8467e274636594ff9b96bff75f3753fb1b221631
Porigon <tsukune398@gmail.com>
2020-10-11 23:45:51 +0900

    back to previous page when push back key

--- app/src/main/java/com/example/gatherlink/MainActivity.kt
+++ app/src/main/java/com/example/gatherlink/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.gatherlink
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,8 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // OMAJINAI
-        webview.webViewClient = WebViewClient()
+        webView.webViewClient = WebViewClient()
         // set URL want to open in WebView
-        webview.loadUrl("http://www.google.com")
+        webView.loadUrl("http://www.google.com")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // when device's back key pressed
+        if(keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            // go back to previous page (not to home screen)
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
