5981504420b3b66741c2557dbe46f56905dcfd6e
Porigon <tsukune398@gmail.com>
2020-10-19 00:12:19 +0900

    output HTML page's title

--- app/src/main/java/com/example/gatherlink/MainActivity.kt
+++ app/src/main/java/com/example/gatherlink/MainActivity.kt
@@ -1,23 +1,63 @@
 package com.example.gatherlink
 
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 /**
 　* Main Activity.
  */
 class MainActivity : AppCompatActivity() {
+    /** URL of page show in WebView at first */
+    val WEB_VIEW_URL = "http://www.google.com"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // set URL & show WebView
+        showWebView()
+        // scrape & parse HTML
+        scrapeHtml()
+    }
+
+    /**
+    * set URL & show WebView.
+    */
+    private fun showWebView() {
         // OMAJINAI
         webView.webViewClient = WebViewClient()
         // set URL want to open in WebView
-        webView.loadUrl("http://www.google.com")
+        webView.loadUrl(WEB_VIEW_URL)
+    }
+
+    /**
+     * scrape & parse HTML.
+     */
+    private fun scrapeHtml() {
+        /** DOM(?) */
+        lateinit var doc: Document
+        /** HTML page's title */
+        var title = ""
+
+        // use coroutine for HTTP communication
+        launch {
+            // fetch and parse HTML file
+            doc = Jsoup.connect(WEB_VIEW_URL).get()
+        }
+        // wait 2000ms
+        Thread.sleep(2000)
+
+        // get HTML page's title from DOM
+        title = doc.title()
+        // output HTML page's title
+        Log.d("hoge", title)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
