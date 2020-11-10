d982b84e63281d70aa7a74dab949f53432c28fe9
Porigon <tsukune398@gmail.com>
2020-11-11 02:58:25 +0900

    display LinkList

--- app/src/main/java/com/example/gatherlink/MainActivity.kt
+++ app/src/main/java/com/example/gatherlink/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.example.gatherlink
 
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.launch
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 
 /**
 　* Main Activity.
  */
 class MainActivity : AppCompatActivity() {
-    /** URL of the page show in WebView */
-    private var mWebViewUrl = "https://google.com"
-    /** LinkList Fragment instance */
-    private lateinit var fragment: Fragment
-    /** Fragment Transaction */
-    private lateinit var transaction: FragmentTransaction
-    /** if LinkList is displayed */
+    /** WebView. */
+    lateinit var mWebView: WebView
+    /** LinkListFragment instance. */
+    private lateinit var mFragment: Fragment
+    /** Fragment Transaction. */
+    private lateinit var mTransaction: FragmentTransaction
+
+    /** URL of the page show in WebView. */
+    var mWebViewUrl = "https://google.com"
+    /** if LinkList is displayed. */
     private var mIsDisplayingLinkList = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +33,8 @@ class MainActivity : AppCompatActivity() {
 
         // set a tool bar
         setSupportActionBar(findViewById(R.id.toolBar))
+        // WebView
+        mWebView = findViewById(R.id.webView)
 
         // set URL & show WebView
         showWebView()
@@ -53,13 +55,10 @@ class MainActivity : AppCompatActivity() {
             // when LinkList isn't displayed
             if(!mIsDisplayingLinkList) {
                 // make Fragment
-                fragment = LinkListFragment()
+                mFragment = LinkListFragment()
 
                 // let LinkListFragment slide in
                 startSlideIn()
-
-                // scrape & parse HTML
-                scrapeHtml()
             } else {
                 // let LinkListFragment slide out
                 startSlideOut()
@@ -77,7 +76,7 @@ class MainActivity : AppCompatActivity() {
                 startSlideOut()
             }
             // go back to the previous page (not to the home screen)
-            webView.goBack()
+            mWebView.goBack()
             return true
         }
         return super.onKeyDown(keyCode, event)
@@ -88,35 +87,9 @@ class MainActivity : AppCompatActivity() {
      */
     private fun showWebView() {
         // OMAJINAI
-        webView.webViewClient = WebViewClient()
+        mWebView.webViewClient = WebViewClient()
         // set URL want to open in WebView
-        webView.loadUrl(mWebViewUrl)
-    }
-
-    /**
-     * scrape & parse HTML.
-     */
-    private fun scrapeHtml() {
-        // DOM(?)
-        lateinit var doc: Document
-
-        // get current page's URL
-        mWebViewUrl = webView.url
-
-        // use a coroutine for HTTP communication
-        launch {
-            // fetch and parse HTML file
-            doc = Jsoup.connect(mWebViewUrl).get()
-        }
-        // wait 2000ms
-        Thread.sleep(5000)
-
-        // get "a-tag" from DOM
-        val newsHeadlines = doc.select("a")
-        // I DON'T UNDERSTAND but can get some URL
-        for(headline in newsHeadlines) {
-            Log.d("hoge", headline.absUrl("href"))
-        }
+        mWebView.loadUrl(mWebViewUrl)
     }
 
     /**
@@ -124,15 +97,15 @@ class MainActivity : AppCompatActivity() {
      * */
     private fun startSlideIn() {
         // use FragmentTransaction
-        transaction = supportFragmentManager.beginTransaction()
+        mTransaction = supportFragmentManager.beginTransaction()
 
         // set animations for Fragment's transition
-        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right,
+        mTransaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right,
             android.R.anim.slide_in_left, android.R.anim.slide_out_right)
         // add Fragment
-        transaction.add(R.id.webView, fragment)
+        mTransaction.add(R.id.webView, mFragment)
         // adjust Fragment transaction
-        transaction.commit()
+        mTransaction.commit()
 
         // set a flag...
         mIsDisplayingLinkList = true
@@ -143,15 +116,15 @@ class MainActivity : AppCompatActivity() {
      * */
     private fun startSlideOut() {
         // use FragmentTransaction
-        transaction = supportFragmentManager.beginTransaction()
+        mTransaction = supportFragmentManager.beginTransaction()
 
         // set animations for Fragment's transition
-        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right,
+        mTransaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right,
             android.R.anim.slide_in_left, android.R.anim.slide_out_right)
         // remove Fragment
-        transaction.remove(fragment)
+        mTransaction.remove(mFragment)
         // adjust Fragment transaction
-        transaction.commit()
+        mTransaction.commit()
 
         // set a flag...
         mIsDisplayingLinkList = false
